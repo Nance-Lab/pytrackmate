d69f37dae90ca9eb6ebbef22adc68a8783f70a37
Nels Schimek <nlsschim@uw.edu>
2023-05-03 10:57:35 -0700

    working on script

--- pytrackmate/trackmate_mpt_script.py
+++ pytrackmate/trackmate_mpt_script.py
@@ -6,19 +6,19 @@ import os
 import imagej
 import scyjava as sj
 # import random
-sj.config.add_options('-Xmx6g')
+sj.config.add_options('-Xmx12g')
 
-os.environ["JAVA_HOME"] = "/Users/nelsschimek/mambaforge/envs/i2k-2022-pyimagej/"
+os.environ["JAVA_HOME"] = "/Users/nelsschimek/anaconda3/envs/pytrackmate/"
 
 # initialize ImageJ
 ij = imagej.init('/Applications/Fiji.app/')
 print(f"ImageJ version: {ij.getVersion()}")
 
-plga_peg_image_path = "/Users/nelsschimek/Documents/nancelab/diff_classifier/notebooks/development/MPT_Data/P10F_NT_10DIV_40nm_slice_2_midbrain_vid_2_0_0.tif"
-plga_peg_image = ij.io().open(plga_peg_image_path)
+tif_file = "/Users/nelsschimek/Documents/nancelab/60X_OGD_Striatum_Slice3_Video4.tif"
+tif_file = ij.io().open(tif_file)
 
 print(
-    f" dims: {plga_peg_image.dims if hasattr(plga_peg_image, 'dims') else 'N/A'}")
+    f" dims: {tif_file.dims if hasattr(tif_file, 'dims') else 'N/A'}")
 
 Model = sj.jimport('fiji.plugin.trackmate.Model')
 Settings = sj.jimport('fiji.plugin.trackmate.Settings')
@@ -41,7 +41,7 @@ model = Model()
 
 model.setLogger(Logger.IJ_LOGGER)
 
-imp = ij.py.to_imageplus(plga_peg_image)
+imp = ij.py.to_imageplus(tif_file)
 
 dims = imp.getDimensions() # default order: XYCZT
 
